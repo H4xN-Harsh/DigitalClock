302c5724bdeb111a421e3f88aabbaee0e1140bf3
H4xN-Harsh <harshxvillen74@gmail.com>
2025-07-25 12:54:23 +0530

    digital clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState,useEffect} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [time,setTime] = useState(new Date());
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setTime(new Date());
+    },1000);
+    return ()=>clearInterval(interval);
+  },[]);
+  const formattedTime = time.toLocaleTimeString();
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className=' w-full h-screen bg-gray-500 flex items-center justify-center'>
+      <h1 className='text-[300px] font-extrabold'>{formattedTime}</h1>
+    </div>
   )
 }
 
